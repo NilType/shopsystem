5f60aac79fda2a2e632d4b5e2985d599cac49e99
typenil <pengpwltype@163.com>
2015-11-25 00:12:38 +0800

    建立用户表

--- shopsystem/apps/catalog/models.py
+++ shopsystem/apps/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    UseId = models.AutoField(primary_key=True, unique=True, null=False)
+    UserName = models.CharField(max_length=20, unique=True, null=False)
+    password = models.CharField(max_length=50, null=False)
+    registerTime = models.DateTimeField(auto_now=True, null=False)
+    loginTime = models.DateTimeField(auto_now=True, null=False)
